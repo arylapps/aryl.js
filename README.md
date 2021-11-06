9ff055f3a844b0c7f0b1be41a3600e9e1a4e1fc8
William Gao <wlgao@ucsc.edu>
2021-11-05 23:19:26 -0700

    Create engine package

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+
+Remove all `node_modules` folders in each package:
+```
+lerna clean
+```
+
+Link and install dependencies:
+```
+lerna bootstrap
+```
+
+Add a dependency:
+```
+lerna add <dep> --scope
+```
+
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -7,8 +7,7 @@ import {
     FreeCamera,
     HemisphericLight,
     Mesh,
-    MeshBuilder,
-    RenderTargetTexture,
+    MeshBuilder, Ray, RayHelper, RenderTargetTexture,
     Scene,
     ShadowGenerator,
     StandardMaterial,
@@ -24,12 +23,29 @@ import {
 
 const createScene = (engine: Engine) => {
     const scene = new Scene(engine);
-    scene.clearColor = Color4.FromHexString("#E7FAFFFF");
+    scene.clearColor = Color4.FromHexString("#cfebfdff");
 
     const sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
-    sphere.position.y = 0;
+    // sphere.position.x = 10;
+    sphere.position.y = 1;
+    const sphereMat = new StandardMaterial("sphereMat", scene);
+    sphereMat.diffuseColor = Color3.FromHexString("#91ddbc")
+    sphereMat.specularColor = Color3.Black();
+    sphere.material = sphereMat;
 
-    const ground = Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    const moveSphere = (counter: number) => {
+        sphere.position.x = Math.sin(counter) * 10;
+        sphere.position.z = Math.cos(counter) * 10;
+    }
+
+
+    const ground = Mesh.CreateGround("ground1", 25, 25, 2, scene);
+    const groundMat = new StandardMaterial("groundMat", scene);
+    groundMat.diffuseColor = Color3.White();
+    // groundMat.specularColor = Color3.White();
+    // groundMat.specularPower = 5;
+    ground.material = groundMat;
+    ground.receiveShadows = true;
 
     // Set up camera
     // const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
@@ -46,48 +62,75 @@ const createScene = (engine: Engine) => {
     ambient.diffuse = new Color3(0.5, 0.5, 0.5);
     ambient.specular = new Color3(0, 0, 0);
     ambient.groundColor = new Color3(0.4, 0.4, 0.4);
-    ambient.intensity = 0.5;
+    ambient.intensity = 0.2;
 
-    const light = new DirectionalLight("light", new Vector3(-1, -2, -1), scene);
-    light.position = new Vector3(12, 15, 5);
+    // const light = new PointLight("light", Vector3.Zero(), scene);
+    const light = new DirectionalLight("light", new Vector3(0, -1, 0), scene);
+    light.position = new Vector3(12, 15, 5)
     light.diffuse = new Color3(1, 1, 1);
-    light.intensity = 1;
+    // light.specular = Color3.Black();
+    light.intensity = 0.8;
 
     const lightSphere = Mesh.CreateSphere("lightSphere", 10, 2, scene);
     lightSphere.position = light.position;
-    const material0 = new StandardMaterial("light", scene);
-    material0.emissiveColor = new Color3(1, 1, 0);
-    lightSphere.material = material0;
+    const lightSphereMat = new StandardMaterial("lightSphereMat", scene);
+    lightSphereMat.emissiveColor = new Color3(1, 1, 0);
+    lightSphere.material = lightSphereMat;
+
+    const moveLight = (counter: number) => {
+        light.position.x = Math.sin(counter) * 6;
+        light.position.z = Math.cos(counter) * 6;
+
+        // target - src
+        light.direction = sphere.position.clone()
+            .subtract(light.position)
+            .normalize()
+        light.direction.y = -1;
+    }
+
 
     const shadowGen = new ShadowGenerator(512, light);
     const shadowMap = shadowGen.getShadowMap()
     if (shadowMap) {
-        shadowMap.refreshRate = RenderTargetTexture.REFRESHRATE_RENDER_ONCE;
+        // shadowMap.refreshRate = RenderTargetTexture.REFRESHRATE_RENDER_ONCE;
 
         shadowMap.renderList?.push(sphere)
-        ground.receiveShadows = true;
     }
-    shadowGen.filteringQuality = ShadowGenerator.QUALITY_LOW;
+    // shadowGen.filteringQuality = ShadowGenerator.QUALITY_LOW;
     shadowGen.setDarkness(0.6);
     shadowGen.useBlurExponentialShadowMap = true;
     shadowGen.useKernelBlur = true;
     shadowGen.blurKernel = 32;
 
     // Draw a grid
-    const grid = MeshBuilder.CreateGround("grid", {width: 20, height: 20}, scene);
-    const material1 = new GridMaterial("grid", scene);
-    material1.backFaceCulling = false;
-    material1.gridRatio = 1;
-    material1.mainColor = new Color3(0.6, 0.6, 0.6);
-    material1.lineColor = new Color3(0.7, 0.7, 0.7);
-    material1.opacity = 0.3;
-    material1.freeze();
-    grid.material = material1;
+    const grid = MeshBuilder.CreateGround("grid", {width: 25, height: 25}, scene);
+    const gridMat = new GridMaterial("gridMat", scene);
+    gridMat.zOffset = -1;
+    gridMat.backFaceCulling = false;
+    gridMat.gridRatio = 1;
+    gridMat.majorUnitFrequency = 2;
+    // gridMat.mainColor = new Color3(0.6, 0.6, 0.6);
+    gridMat.lineColor = new Color3(0.7, 0.7, 0.7);
+    gridMat.opacity = 0.3;
+    gridMat.freeze();
+    grid.material = gridMat;
     grid.isPickable = false;
     grid.doNotSyncBoundingInfo = true;
     // grid.convertToUnIndexedMesh();
     grid.freezeWorldMatrix();
     grid.freezeNormals();
+
+
+    let counter = 0;
+    const offset = 50;
+
+    engine.runRenderLoop(() => {
+        scene.render();
+        counter += 0.01;
+
+        // moveSphere(counter / 10 + offset);
+        moveLight(counter);
+    });
 
     return scene;
 }
@@ -98,6 +141,6 @@ const engine = new Engine(canvas);
 const scene = createScene(engine);
 
 
-engine.runRenderLoop(() => {
-    scene.render();
-});
+// engine.runRenderLoop(() => {
+//     scene.render();
+// });
